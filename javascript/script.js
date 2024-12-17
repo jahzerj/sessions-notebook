cabd340168689c0d138c90d5fc7e286f7d19b699
Jahaziel <jahzerahj@gmail.com>
2024-12-17 10:10:18 +0100

    working with objects

--- javascript/script.js
+++ javascript/script.js
@@ -8,4 +8,20 @@ const person = {
   name: "Max Blub",
   age: 24,
   isStudent: true,
+  address: {
+    street: "Ritterstrass",
+    "house-number": 21,
+    city: "Berlin",
+  },
 };
+
+console.log("person name", person.name);
+console.log("person age", person.age);
+
+// console.log("person name", person[name])
+console.log("person name", person["name"]);
+
+const houseNumber = person.address["house-number"];
+person.address["house-number"] = 23;
+
+// nested objects work too

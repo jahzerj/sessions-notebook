222219178959ad202bb217a6b9ec565702cf93a7
Jahaziel <jahzerahj@gmail.com>
2024-12-17 09:46:19 +0100

    coding along

---
+++ javascript/index.js
@@ -0,0 +1,11 @@
+let value;
+
+value = true;
+// value = false;
+
+if (value) {
+    console.log('Value is truthy')
+} else{
+    console.log("Value is fals");
+    
+}
---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+const name = "Max Blub";
+
+const isStudent = true;
+
+const age = 24;
+
+const person = {
+  name: "Max Blub",
+  age: 24,
+  isStudent: true,
+};
